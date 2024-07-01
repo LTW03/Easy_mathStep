e9aa7edcf5fe1c33f5e4f85d59dc839d96909274
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-07-01 20:19:47 +0800

    library page update

--- library_page.php
+++ library_page.php
@@ -1,7 +1,22 @@
-<?php
-include('teacher_base.html')
-?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Css_folder/teacher_base.css">
+      <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+      <script defer src="JS_Folder/teacher_base.js"></script> 
+    <title>Library | E.M.S</title>
+</head>
+<body>
+    <?php
+    include('teacher_base.html')
+    ?>
 
-<p>
-    hello world
-</p>
+    <div class= "quizgame_container"> <!--item container-->
+        
+
+    </div>
+    
+</body>
+</html>
