d4d5fe1cb29fe45ff470f4409dd13311f0388bc6
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-07-01 16:51:45 +0800

    teacher page base
    responsive sidebar and header bar

---
+++ JS_Folder/teacher_base.js
@@ -0,0 +1,27 @@
+/*==================== SHOW MENU ====================*/
+const showMenu = (headerToggle, navbarId) =>{
+    const toggleBtn = document.getElementById(headerToggle),
+    nav = document.getElementById(navbarId)
+    
+    // Validate that variables exist
+    if(headerToggle && navbarId){
+        toggleBtn.addEventListener('click', ()=>{
+            // We add the show-menu class to the div tag with the nav__menu class
+            nav.classList.toggle('show-menu')
+
+            //change icon
+            toggleBtn.classList.toggle('bx-x')
+        })
+    }
+}
+showMenu('header-toggle','navbar')
+
+//   link active
+const linkColor = document.querySelectorAll('.nav-link')
+
+function colorLink(){
+    linkColor.forEach(l => l.classList.remove('active'))
+    this.classList.add('active')
+}
+
+linkColor.forEach(l => l.addEventListener('click',colorLink))
