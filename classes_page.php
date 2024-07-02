73a51e5f280b435e0da536f7f00b5f12e7d61fec
LTW03 <108678641+LTW03@users.noreply.github.com>
2024-07-02 15:29:47 +0800

    new page added and teacher base update
    add new page of setting and classes
    link each page to teacher base.

---
+++ classes_page.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Css_folder/teacher_base.css">
+    <link rel="stylesheet" href="Css_folder/library_page.css">
+    <!-- <link rel="stylesheet" href="Css_folder/general.css"> -->
+
+      <link rel="stylesheet"href="https://cdn.jsdelivr.net/npm/boxicons@latest/css/boxicons.min.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" />
+
+      <script defer src="JS_Folder/teacher_base.js"></script> 
+    <title>Library | E.M.S</title>
+</head>
+<body>
+    <?php
+    include('teacher_base.html')
+    ?>
+
+</body>
+</html>
